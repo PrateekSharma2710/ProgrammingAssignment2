8c2ba0775ec99c6959a0bc5f5e9252fdd59014da
Prateek Sharma <prateeksharma2710@gmail.com>
2016-08-27 09:47:28 +0800

    Add files via upload
    Rprogrammingassignment#2

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,23 @@
 ## Put comments here that give an overall description of what your
-## functions do
+## functions do. CacheMatrix Produces a matrix which cache its inverse
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+## Initialize invr as a litmus paper test for tagging absence or presence in cache
+    invr <- NULL
+    set <- function(z) {
+    x <<- z
+    invr <<- NULL
+  }
+  get <- function() x
+  setinvrse <- function(invrse) invr <<- invrse
+  getinvrse <- function() invr
+  list(set = set,
+       get = get,
+       setinvrse = setinvrse,
+       getinvrse = getinvrse)
 
 }
 
@@ -12,4 +26,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+    invr <- x$getinvrse()
+    if (!is.null(invr)) {
+      message("get cached data")
+      return(invr)
+    }
+    
+    matrx <- x$get()
+    invr <- solve(matrx, ...)
+    x$setinvrse(invr)
+    invr
+  
 }
